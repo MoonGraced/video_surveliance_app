de9beeffabf048ffe029cc4190bb09b10326b7da
Kirill Gerasimov <talathel@vk.com>
2025-05-08 13:52:06 +0300

    basic CMakeLists.txt

---
+++ build/CMakeFiles/video_surveliance_app.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/video_surveliance_app.dir/link.d"
+  "CMakeFiles/video_surveliance_app.dir/src/source_files/main.cpp.o"
+  "CMakeFiles/video_surveliance_app.dir/src/source_files/main.cpp.o.d"
+  "video_surveliance_app"
+  "video_surveliance_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/video_surveliance_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
